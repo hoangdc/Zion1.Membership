f12cdd0e33dd868a69254ef3e7bf2f8970c52f89
Hoang Dinh <hoangdc@gmail.com>
2023-10-06 22:24:10 -0500

    add group page

--- Zion1.Membership.Application/DTOs/MemberDto.cs
+++ Zion1.Membership.Application/DTOs/MemberDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Zion1.Membership.Application.DTOs
+﻿namespace Zion1.Membership.Application.DTOs
 {
     public class MemberDto
     {
